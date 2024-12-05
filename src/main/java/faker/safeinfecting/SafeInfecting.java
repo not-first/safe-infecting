71bca8b1dc0f3734c9742f2ec1f729c73b6b2e0a
fakerr <111339712+not-first@users.noreply.github.com>
2024-12-05 11:56:40 +1100

    initial files

---
+++ src/main/java/faker/safeinfecting/SafeInfecting.java
@@ -0,0 +1,16 @@
+package faker.safeinfecting;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SafeInfecting implements ModInitializer {
+	public static final String MOD_ID = "safe-infecting";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Safe Infecting initialized.");
+	}
+}
---
+++ src/main/java/faker/safeinfecting/mixin/SafeInfectingMixin.java
@@ -0,0 +1,23 @@
+package faker.safeinfecting.mixin;
+
+import net.minecraft.entity.mob.ZombieEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.Difficulty;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ZombieEntity.class)
+public class SafeInfectingMixin {
+    @Redirect(
+            method = "onKilledOther",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/server/world/ServerWorld;getDifficulty()Lnet/minecraft/world/Difficulty;"
+            )
+    )
+    // hard mode has a 100% chance of conversion, so replace the difficulty check with a constant return of hard
+    private Difficulty alwaysReturnHard(ServerWorld instance) {
+        return Difficulty.HARD;
+    }
+}
