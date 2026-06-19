e8ba941d2f195cc4f53b634ee788c98d8b8b116d
fakerr <111339712+not-first@users.noreply.github.com>
2026-06-19 21:50:04 +1000

    added support for 26.1 through 26.1.2

--- src/main/java/faker/safeinfecting/mixin/SafeInfectingMixin.java
+++ src/main/java/faker/safeinfecting/mixin/SafeInfectingMixin.java
@@ -1,23 +1,24 @@
 package faker.safeinfecting.mixin;
 
-import net.minecraft.entity.mob.ZombieEntity;
-import net.minecraft.server.world.ServerWorld;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.Difficulty;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.monster.zombie.Zombie;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(ZombieEntity.class)
+@Mixin(Zombie.class)
 public class SafeInfectingMixin {
+
 	@Redirect(
-			method = "onKilledOther",
+			method = "killedEntity",
 			at = @At(
 					value = "INVOKE",
-					target = "Lnet/minecraft/server/world/ServerWorld;getDifficulty()Lnet/minecraft/world/Difficulty;"
+					target = "Lnet/minecraft/server/level/ServerLevel;getDifficulty()Lnet/minecraft/world/Difficulty;"
 			)
 	)
-	// hard mode has a 100% chance of conversion, so replace the difficulty check with a constant return of hard
-	private Difficulty alwaysReturnHard(ServerWorld instance) {
+	private Difficulty forceHardDifficulty(ServerLevel level) {
 		return Difficulty.HARD;
 	}
 }
