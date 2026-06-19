43bd920f7f0957845400d17365054798ae15c626
fakerr <111339712+not-first@users.noreply.github.com>
2026-06-19 21:56:24 +1000

    added support for 26.2, removed uneeded import from mixin, update github action

--- src/main/java/faker/safeinfecting/mixin/SafeInfectingMixin.java
+++ src/main/java/faker/safeinfecting/mixin/SafeInfectingMixin.java
@@ -2,7 +2,6 @@ package faker.safeinfecting.mixin;
 
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.Difficulty;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.monster.zombie.Zombie;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
